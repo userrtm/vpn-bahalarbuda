416637fe824010c60baf0f77ee9c013d08f0c2a0
userrtm <masterbigserverclas@gmail.com>
2026-05-28 14:35:35 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, "0.0.0.0");
+app.get("/:page", (req, res) => {
+  const page = req.params.page;
+
+  if (page.includes(".")) {
+    return res.sendFile(path.join(__dirname, page));
+  }
+
+  res.sendFile(path.join(__dirname, page + ".html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("UserrTM site running on port " + PORT);
+});
